e47c3de6809c19ea4e9f5b221a819f277df54a80
Sami TEMIMI <63162617+Dz-LevelZero@users.noreply.github.com>
2021-11-17 08:40:31 +0100

    Update Routes.jsx

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
-import Results from './Results'
+import { Results } from './Results';
 
 const Routing = () => {
   return (
